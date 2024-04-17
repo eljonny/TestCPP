b02e02b1024cceb769305062f34cfc665bc310c2
Jonathan Hyry <el.jonny@gmail.com>
2024-04-16 21:12:43 -0700

    Fix suppression of MSVC warning C4710.
    Only applies to Release builds, and applies whether stacktraces are
    enabled or not.

--- cmake/BuildTypeHandling.cmake
+++ cmake/BuildTypeHandling.cmake
@@ -1,7 +1,7 @@
 list (
     APPEND
     MSVC_RELEASE_BUILD_OPTS
-    /Wall /WX /O2 /wd4820
+    /Wall /WX /O2 /wd4710 /wd4820
 )
 list (
     APPEND
@@ -13,12 +13,12 @@ if (${TESTCPP_STACKTRACE_ENABLED})
     list (
         APPEND
         MSVC_RELEASE_BUILD_OPTS
-        /wd4710 /wd4668 /wd5039 /wd4625 /wd4626 /wd5026 /wd5027
+        /wd4668 /wd5039 /wd4625 /wd4626 /wd5026 /wd5027
     )
     list (
         APPEND
         MSVC_DEBUG_BUILD_OPTS 
-        /wd4710 /wd4668 /wd5039 /wd4625 /wd4626 /wd5026 /wd5027 /wd5045
+        /wd4668 /wd5039 /wd4625 /wd4626 /wd5026 /wd5027 /wd5045
         /Qspectre
     )
 endif ()
