06faf3639137cbe9db4f482e26b19a5a61940ae6
Jonathan Hyry <el.jonny@gmail.com>
2024-04-16 20:41:35 -0700

    It looks like some issues only occur in the stacktrace-enabled build.
    Adding warning suppressions for build that includes boost::stacktrace,
    it is causing internal Windows SDK warnings that are causing the build
    to fail.
    Moved all the warning suppressions to addition only when stacktraces are
    enabled since it seems those are caused by boost::stacktrace.

--- cmake/BuildTypeHandling.cmake
+++ cmake/BuildTypeHandling.cmake
@@ -1,13 +1,27 @@
 list (
     APPEND
     MSVC_RELEASE_BUILD_OPTS
-    /Wall /WX /O2 /wd4710 /wd4820 /wd4668 /wd5039
+    /Wall /WX /O2
 )
 list (
     APPEND
     MSVC_DEBUG_BUILD_OPTS 
-    /Wall /WX /Od /wd4710 /wd4820 /wd4668 /wd5039
+    /Wall /WX /Od
 )
+
+if (${TESTCPP_STACKTRACE_ENABLED})
+    list (
+        APPEND
+        MSVC_RELEASE_BUILD_OPTS
+        /wd4710 /wd4820 /wd4668 /wd5039 /wd4625 /wd4626 /wd5026
+    )
+    list (
+        APPEND
+        MSVC_DEBUG_BUILD_OPTS 
+        /wd4710 /wd4820 /wd4668 /wd5039 /wd4625 /wd4626 /wd5026
+    )
+endif ()
+
 list (
     APPEND
     GCC_CLANG_RELEASE_BUILD_OPTS
