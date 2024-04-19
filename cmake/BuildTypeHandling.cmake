45bb01af3049840ce697dd0a663b83c803e5a855
Jonathan Hyry <el.jonny@gmail.com>
2024-04-18 21:10:18 -0700

    Still trying to disable MSVC warning C4710

--- cmake/BuildTypeHandling.cmake
+++ cmake/BuildTypeHandling.cmake
@@ -13,7 +13,7 @@ if (${TESTCPP_STACKTRACE_ENABLED})
     list (
         APPEND
         MSVC_RELEASE_BUILD_OPTS
-        /wd4668 /wd5039 /wd4625 /wd4626 /wd5026 /wd5027
+        /wd4710 /wd4668 /wd5039 /wd4625 /wd4626 /wd5026 /wd5027
     )
     list (
         APPEND
