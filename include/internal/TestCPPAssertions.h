bb4ac7f094fc2002b75f8451b952e640a180c143
Jonathan Hyry <el.jonny@gmail.com>
2024-05-13 08:06:03 -0700

    Major refactor and API change
    I broke up TestCPP.h and the corresponding .cpp file into many pieces.
    It just started to get too big, and I wanted more focused TUs.
    So this is the first commit for that, where I split them out into
    different header files, put them in an 'internal' subdirectory, and
    turned TestCPP.h into an aggregate include for including the types and
    definitions needed by the library.
    So the common definitions are now split out into their own header-only
    definition in internal/TestCPPCommon.h.
    The exception types are now in their own internal/TestCPPExceptions.h
    header with corresponding .cpp.
    TestCase and TestSuite now have their own headers,
    internal/TestCPPTestCase.h and internal/TestCPPTestSuite.h, with
    corresponding .cpps.
    And finally TestCPPUtil.h is moved into internal and the TestObjName
    type has been moved into the Util header and definition .cpp, but is
    still in the base namespace TestCPP, not in TestCPP::Util.
    
    The biggest functional change is splitting the assertions out of
    TestSuite and into their own Assertions class, in their own TU.
    So from now on, the TestCPP assertions should be referenced through the
    Assertions class, not the TestSuite class, which makes a lot more
    sense.
    I added a couple new assertions a few weeks back, and I can see adding
    more, so they really need their own type to encapsulate that
    functionality.
    
    There's some significant cleanup to do, but that will probably be in a
    separate PR.

---
+++ include/internal/TestCPPAssertions.h
@@ -0,0 +1,251 @@
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+ */
+
+#ifndef TESTCPP_ASSERTIONS_
+#define TESTCPP_ASSERTIONS_
+
+#include "TestCPPExceptions.h"
+#include "TestCPPTestCase.h"
+
+/**
+ * The base namespace for all TestCPP library code.
+ */
+namespace TestCPP {
+
+    /**
+     * @class Assertions
+     * @author Jonathan Hyry
+     * @date 11/05/24
+     * @file TestCPPAssertions.h
+     * @brief Contains the TestCPP library's assertions.
+     * 
+     * Defines some assertions here, where they are templated.
+     * Declares the rest.
+     * 
+     * This is the place where the TestCPP Assertions API is defined.
+     */
+    class Assertions {
+    public:
+        /**
+         * @brief Check that something equals something else using the
+         *          built-in operator== for each type.
+         * @param expected The value that the actual value should be
+         *                  equivalent to.
+         * @param actual The actual value that will be checked against
+         *                  the expected value.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        template<typename T1, typename T2>
+        static void assertEquals (
+                T1 expected, T2 actual,
+                string failureMessage = "Arguments are not equivalent!"
+            )
+        {
+            if (expected != actual) {
+                stringstream err;
+
+                err << "Equivalence assertion failed!" << endl;
+                err << failureMessage << endl;
+                err << "Expected: <" << expected << ">" << endl;
+                err << "Actual: <" << actual << ">" << endl;
+
+                throw TestFailedException(err.str());
+            }
+        }
+
+        /**
+         * @brief Check that something is not equivalent to something
+         *          else using the built-in operator== for each type.
+         * @param expected The value that the actual value should not be
+         *                  equivalent to.
+         * @param actual The actual value that will be checked against
+         *                  the expected value.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        template<typename T1, typename T2>
+        static void assertNotEquals (
+                T1 expected, T2 actual,
+                string failureMessage = "Arguments are equivalent!"
+            )
+        {
+            if (expected == actual) {
+                stringstream err;
+
+                err << "Non-Equivalence assertion failed!" << endl;
+                err << failureMessage << endl;
+                err << "Expected: <" << expected << ">" << endl;
+                err << "Actual: <" << actual << ">" << endl;
+
+                throw TestFailedException(err.str());
+            }
+        }
+
+        /**
+         * @brief Check that a pointer is null.
+         * @param ptr The pointer to check.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        template<typename T>
+        static void assertNull (
+                T ptr,
+                string failureMessage = "Object is not null!"
+            )
+        {
+            bool null = ptr == nullptr;
+
+            if (!null) {
+                stringstream err;
+
+                err << "Null assertion failed!" << endl;
+                err << failureMessage << endl;
+
+                throw TestFailedException(err.str());
+            }
+        }
+
+        /**
+         * @brief Check that a pointer is non-null.
+         * @param ptr The pointer to check.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        template<typename T>
+        static void assertNotNull (
+                T ptr,
+                string failureMessage = "Object is null!"
+            )
+        {
+            bool notNull = ptr != nullptr;
+
+            if (!notNull) {
+                stringstream err;
+
+                err << "Not Null assertion failed!" << endl;
+                err << failureMessage << endl;
+
+                throw TestFailedException(err.str());
+            }
+        }
+
+        /**
+         * @brief Verify that a function throws something.
+         * @param shouldThrow The function to check, to ensure that it
+         *                      throws something.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        static void assertThrows (
+            function<void()> shouldThrow,
+            string failureMessage =
+                "Should have thrown something!"
+        );
+
+        /**
+         * @brief Verify that a function does not throw something.
+         * @param shouldThrow The function to check, to ensure that it
+         *                      does not throw something.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        static void assertNoThrows (
+            function<void()> shouldNotThrow,
+            string failureMessage =
+                "Should not have thrown anything!"
+        );
+
+        /**
+         * @brief Verify that a logical condition is true.
+         * @param condition The result of the logical condition to
+         *                      check.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        static void assertTrue (
+            bool condition,
+            string failureMessage = "Condition is false!"
+        );
+
+        /**
+         * @brief Verify that a logical condition is not true.
+         * @param condition The result of the logical condition to
+         *                      check.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         */
+        static void assertFalse (
+            bool condition,
+            string failureMessage = "Condition is true!"
+        );
+
+        /**
+         * @brief Force a test to fail.
+         * @param failureMessage Failure message that should be logged
+         *                          if the assertion fails. This
+         *                          defaults to a generic failure
+         *                          message related to the assertion
+         *                          type.
+         *
+         * Useful in certain situations when:
+         *  - Generating code for test cases, to ensure that skeletons
+         *      fail by default.
+         *  - Forcing test failure in certain circumstances where there
+         *      is nothing to assert but a certain code path is taken.
+         */
+        static void fail (
+            string failureMessage = "Forced test failure!"
+        );
+    };
+}
+
+#endif
---
+++ include/internal/TestCPPExceptions.h
@@ -0,0 +1,102 @@
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+ */
+
+#ifndef TESTCPP_EXCEPTIONS_
+#define TESTCPP_EXCEPTIONS_
+
+#include <stdexcept>
+#include <string>
+
+using std::runtime_error;
+using std::string;
+
+/**
+ * The base namespace for all TestCPP library code.
+ */
+namespace TestCPP {
+
+    /**
+     * @class TestCPPException
+     * @author Jonathan Hyry
+     * @date 03/05/24
+     * @file TestCPP.h
+     * @brief Provides a custom base exception for failure conditions.
+     * 
+     * There are two types of failures in the library:
+     *  - Errors caused by bugs, system problems, etc.
+     *  - Test failures
+     * 
+     * This type, when used directly, is for representing the first type
+     *  of failure: errors in the library caused by bugs or other
+     *  problems.
+     */
+    class TestCPPException : public runtime_error {
+    public:
+        /**
+         * Construct an exception of this type with a string literal for
+         *  its failure message.
+         */
+        TestCPPException (const char * msg);
+
+        /**
+         * Construct an exception of this type with a string object for
+         *  its failure message.
+         */
+        TestCPPException (string&& msg);
+    };
+
+    /**
+     * @class TestFailedException
+     * @author Jonathan Hyry
+     * @date 03/05/24
+     * @file TestCPP.h
+     * @brief Provides an exception type specifically for test failures.
+     * 
+     * There are two types of failures in the library:
+     *  - Errors caused by bugs, system problems, etc.
+     *  - Test failures
+     * 
+     * This type is for representing the second type of failure: test
+     *  failures.
+     */
+    class TestFailedException : public TestCPPException {
+    public:
+        /**
+         * Construct an exception of this type with a string literal for
+         *  its failure message.
+         */
+        TestFailedException (const char * msg);
+
+        /**
+         * Construct an exception of this type with a string literal for
+         *  its failure message.
+         */
+        TestFailedException (string&& msg);
+    };
+}
+
+#endif
