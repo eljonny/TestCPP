08cbd868d41221f7c5653553faf0925796f2934e
Jonathan Hyry <el.jonny@gmail.com>
2024-06-22 09:19:55 -0700

    Expand TestSuite test suite
    The following is what was previously tested in TestSuite:
    - Bare construction, no tests, only suite name
    
    The following is now tested, in addition to what was previously tested:
    - All possible construction parameters
      > Suite construction with TestCase objects
      > Suite construction with tuples
      > Suite construction with various mixes of TestCases and tuples
    - TestSuite::enableTestPassedMessage with various mixes of numbers of
       tests.
    - TestSuite::disableTestPassedMessage with various mixes of numbers of
       tests.

--- test/include/TestSuite/TestSuiteSuite.h
+++ test/include/TestSuite/TestSuiteSuite.h
@@ -10,8 +10,64 @@ namespace TestCPP {
                 "TestCPP TestSuite Tests",
 
                 make_tuple(
-                    "Suite construction Test",
-                    function<void()>(TestSuiteTests::TestConstructSuite)
+                    "Suite construction Test - no tests",
+                    function<void()>(
+                   TestSuiteTests::TestConstructSuiteBare
+                    )
+                ),
+                make_tuple(
+                    "Suite construction Test - TestCases",
+                    function<void()>(
+                   TestSuiteTests::TestConstructSuiteTestCases
+                    )
+                ),
+                make_tuple(
+                    "Suite construction Test - tuples",
+                    function<void()>(
+                   TestSuiteTests::TestConstructSuiteTuples
+                    )
+                ),
+                make_tuple(
+                    "Suite construction Test - mixed",
+                    function<void()>(
+                   TestSuiteTests::TestConstructSuiteMixed
+                    )
+                ),
+                make_tuple(
+                    "Suite enable test passed message - no tests",
+                    function<void()>(
+                   TestSuiteTests::TestEnableTestPassedMessageNoTests
+                    )
+                ),
+                make_tuple(
+                    "Suite enable test passed message - one test",
+                    function<void()>(
+                   TestSuiteTests::TestEnableTestPassedMessageOneTest
+                    )
+                ),
+                make_tuple(
+                    "Suite enable test passed message - many tests",
+                    function<void()>(
+                   TestSuiteTests::TestEnableTestPassedMessageManyTests
+                    )
+                ),
+                make_tuple(
+                    "Suite disable test passed message - no tests",
+                    function<void()>(
+                   TestSuiteTests::TestDisableTestPassedMessageNoTests
+                    )
+                ),
+                make_tuple(
+                    "Suite disable test passed message - one test",
+                    function<void()>(
+                   TestSuiteTests::TestDisableTestPassedMessageOneTest
+                    )
+                ),
+                make_tuple(
+                    "Suite disable test passed message - many tests",
+                    function<void()>(
+                   TestSuiteTests::TestDisableTestPassedMessageManyTests
+                    )
                 )
             );
         }
