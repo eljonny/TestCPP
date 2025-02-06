0c7892efb470e78434cc1bc7c1221e0ef1f233ef
Jonathan Hyry <jonathan.hyry@outlook.com>
2025-02-05 19:37:54 -0800

    Update presets so all configurations are covered as buildPresets, fixes
    CMakePresets is now modified such that:
     - there are buildPresets that correspond to all configurationPresets,
        so the proper base native tool options are applied to all relevant
        configurations when running a build in VS or on the command line
     - issues are addressed
     - added descriptions/names where missing
     - made presets hidden where appropriate so they don't show up in the
        VS build configurations menu, and so they can't be referenced when
        the cmake tool is invoked on the command line
    
    Fixed an issue in BuildTypeHandling.cmake where when building with
     ClangCL for running clang-tidy on Windows with (or without) VS, it was
     selecting MSVC++ tool options instead of GCC/Clang tool options since
     it is indeed running an MSBuild build, but the underlying toolchain is
     ClangCL
    
    Added some whitespace where it seemed to make it easier to read.
    
    Added the following gcc/clang switches:
     -Wno-c++98-compat - Because there are C++11 and greater features that
      conflict with C++98 compatibility, but that's ok because the project
      is only intended to be built with C++11 or newer
    
     -Wno-c++98-compat-pedantic - Same reason as for Wno-c++98-compat
    
     -Wno-covered-switch-default - Clang was giving me some confusing
       results... keep the default: case in my switch, and it would output
       this from -Wcovered-switch-default:
        error : default label in switch which covers all enumeration values
         [-Werror,-Wcovered-switch-default]
      Then in rewriting it so there's no default: case but basically works
       the same way, Clang would output this from -Wswitch-default:
        error : 'switch' missing 'default' label [-Werror,-Wswitch-default]
      So I go look it up and find this gem on SE-SoftwareEngineering-design:
        https://lnkd.in/gg8d7U2T
      I opted for defensive programming, as I usually do, left the default:
       case in the switch, and added -Wno-covered-switch-default to my
       build.
      It seems like it's probably the most correct thing to do... at risk
       of having an extremely weird (read: improbable) situation where
       disabling this warning would cause me to miss a bug.
      I think it's more likely that there would be a bug as a result of not
       having a default case to handle erroneous values since enums in
       C/C++ are not strongly-typed (a la Java).
      I'm not using -Weverything, so I find it odd that there are clashes
       in warnings, but it was an interesting exercise to work through this
       issue.
      I would much rather have -Wswitch-default enabled, because that can
       catch some really nasty issues; this is just a weird pedantic
       annoyance, where the default: case is for "just in case" because of
       the lack of type checking, as I mentioned already.
      I did not find arguments on StackOverflow very compelling that argued
       against a default case when all known explicitly-defined cases are
       covered; since enums are value-based types, they can hold any value
       of the underlying type, which the compiler might or might not warn
       about/catch; the collection of known cases is typically a very tiny
       subset of the set of all possible values (4+ billion with the
       default underlying int type), so there should be an escape hatch for
       if the programmer knowningly or unknowingly ventures outside those
       explicitly-defined cases, where it would make sense to do so and a
       sensible default can be used or an error/exception can be generated,
       instead of just crashing the host application with no information
       about why there was a crash; I didn't want to use an assert because
       the values can change and are not tied explicitly to the symbols, so
       an assert could easily be broken.

--- cmake/build/GCCCoverage.cmake
+++ cmake/build/GCCCoverage.cmake
@@ -1,17 +1,25 @@
 list (
     APPEND
     COVERAGE_BUILD_OPTS
-    -g               # Enable all debugging information.
-    -Og              # Ensure the compiler doesn't use optimizations
-                     #  that would harm debuggability of the resulting
-                     #  code.
-    -Wall            # Enable most warnings.
-    -Wextra          # Enable even more warnings.
-    -Wpedantic       # Enable most of the rest of the warnings.
-    -Werror          # Treat all warnings as errors.
-    -fprofile-arcs   # Enable profile points that help with code
-                     #  coverage.
-    -ftest-coverage  # Enable core code coverage compilation.
+    -g                          # Enable all debugging information.
+    -Og                         # Ensure the compiler doesn't use
+                                #  optimizations that would harm
+                                #  debuggability of the resulting code.
+    -Wall                       # Enable most warnings.
+    -Wextra                     # Enable even more warnings.
+    -Wpedantic                  # Enable most of the rest of the
+                                #  warnings.
+    -Werror                     # Treat all warnings as errors.
+    -Wno-c++98-compat           # We're compiling for C++11, so it's not
+                                #  necessary to maintain compatibility
+                                #  with C++98.
+    -Wno-c++98-compat-pedantic  # We're compiling for C++11, so, again,
+                                #  it's not necessary to maintain
+                                #  compatibility with C++98.
+    -Wno-covered-switch-default # -Wswitch-default is more important.
+    -fprofile-arcs              # Enable profile points that help with
+                                #  code coverage.
+    -ftest-coverage             # Enable core code coverage compilation.
 )
 
 target_compile_options (
