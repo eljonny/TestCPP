a8cc19fa0044b4d3d78f5e93320750ec720a70a9
Jonathan Hyry <jonathan.hyry@outlook.com>
2025-06-02 21:31:31 -0700

    Convert Boost.Stacktrace and transitives to git submodules and related
    Added new SubmodulesInit include to top-level CMakeLists.txt.
    This ensures CMake updates all the dependencies that are git submodules
     so everything is at the right tag/commit and exists.
    SubmodulesInit.cmake is fully implemented and derived from
    https://cliutils.gitlab.io/modern-cmake/chapters/projects/submodule.html
    
    No need to conditionally worry about including from Boost.Stacktrace
     since it is now handled through the git submodule dependencies.
    Includes.cmake is altered to take this into account.
    
    Add the required boost_* targets to the install target to ensure the
     necessary files are there to support the library when TestCPP gets
     installed.
    These targets are identified and added to the list in
     Boost.Stacktrace.cmake.
    Formatting.
    
    Update the TestCPP package email contact to the appropriate HSF email
     address.
    
    Implement a more selective and proper Boost.Stacktrace.cmake.
    This version is easier to maintain if the Boost.Stacktrace dependencies
     change in the future.
    It also lays out what targets to install given the environment we're
     building in.

--- cmake/link/Boost.Stacktrace.cmake
+++ cmake/link/Boost.Stacktrace.cmake
@@ -1,14 +1,81 @@
-add_subdirectory(3rdparty/boost.assert)
-add_subdirectory(3rdparty/boost.config)
-add_subdirectory(3rdparty/boost.container_hash)
-add_subdirectory(3rdparty/boost.core)
-add_subdirectory(3rdparty/boost.describe)
-add_subdirectory(3rdparty/boost.mp11)
-add_subdirectory(3rdparty/boost.predef)
-add_subdirectory(3rdparty/boost.stacktrace)
-add_subdirectory(3rdparty/boost.static_assert)
-add_subdirectory(3rdparty/boost.throw_exception)
-add_subdirectory(3rdparty/boost.winapi)
+add_subdirectory(
+    "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/boost.stacktrace"
+    "${CMAKE_CURRENT_BINARY_DIR}/boost.stacktrace"
+    EXCLUDE_FROM_ALL
+)
+
+list(
+    APPEND
+    BOOST_STACKTRACE_DEPS
+
+    assert
+    static_assert
+    throw_exception
+    config
+    container_hash
+    core
+    describe
+    mp11
+    predef
+)
+
+if (MSVC)
+    list(
+        APPEND
+        BOOST_STACKTRACE_DEPS
+
+        winapi
+    )
+endif ()
+
+foreach(B_ST_DEP IN LISTS BOOST_STACKTRACE_DEPS)
+    list (
+        APPEND
+        BOOST_STACKTRACE_EXPORTS_LIST
+        boost_${B_ST_DEP}
+    )
+    add_subdirectory(
+        "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/boost.${B_ST_DEP}"
+        "${CMAKE_CURRENT_BINARY_DIR}/boost.${B_ST_DEP}"
+        EXCLUDE_FROM_ALL
+    )
+endforeach()
+
+list (
+    APPEND
+    BOOST_STACKTRACE_EXPORTS_LIST
+    
+    boost_stacktrace
+)
+
+if (MSVC)
+    list (
+        APPEND
+        BOOST_STACKTRACE_EXPORTS_LIST
+
+        boost_stacktrace_windbg
+    )
+else ()
+    FindBacktrace()
+    if (Backtrace_FOUND)
+        list (
+            APPEND
+            BOOST_STACKTRACE_EXPORTS_LIST
+
+            boost_stacktrace_backtrace
+        )
+    endif ()
+
+    FindBinUtils()
+    if (NOT CMAKE_ADDR2LINE-NOTFOUND)
+        list (
+            APPEND
+            BOOST_STACKTRACE_EXPORTS_LIST
+
+            boost_stacktrace_addr2line
+        )
+    endif ()
+endif ()
 
 target_link_libraries(
     ${PROJECT_NAME}
